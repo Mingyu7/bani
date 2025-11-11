e9ccc0bd8678c6006e42562f9143d79f592ca9b2
mingyu <vkans7526@naver.com>
2025-11-11 12:26:08 +0900

    singup 지디자인변경

--- bani_project/urls.py
+++ bani_project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('users.urls')),
+    path('users/', include('users.urls')),
     path('', include('core.urls')),
     path('weather/', include('weather.urls')),
     path('meal-plan/', include('meal_plans.urls')),
