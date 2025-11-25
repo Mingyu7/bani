a53fd93cd1a48896bbaa3c2c9d74a4d837713857
mingyu <vkans7526@naver.com>
2025-11-26 00:17:55 +0900

    배포용 db 설정

--- bani_project/settings.py
+++ bani_project/settings.py
@@ -97,14 +97,24 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bani',
+        'USER': 'admin',     # 마스터 사용자 이름
+        'PASSWORD': 'Mingyu5749!',
+        'HOST': 'database-2.ct884c60cwin.ap-northeast-2.rds.amazonaws.com', # 엔드포인트
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
